a41c15b840c812bcdb6627bb445719f0d24baec0
Matthew <mkuo@seas.upenn.edu>
2024-04-23 00:44:49 +0000

    Profile page backend without getting embeddings

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,3 +114,97 @@ exports.logout = (req, res) => {
   req.session.username = null;
   return res.status(200).json({ message: 'You were successfully logged out.' });
 };
+
+exports.changePassword = async (req, res) => {
+  const {newPassword, confirmPassword} = req.body;
+  const {user_id} = req.session;
+
+  if (newPassword == null || confirmPassword == null) {
+    return res.status(400).json({error: 'One or more fields were empty.'});
+  }
+
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({error: 'Passwords do not match.'});
+  }
+
+  try {
+    const hashed = await new Promise((resolve, reject) => {
+      bcrypt.hash(newPassword, 10, (err, hash) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(hash);
+        }
+      });
+    });
+    await db.send_sql(
+      `UPDATE users SET password = '${hashed}' WHERE user_id = ${user_id}`
+    );
+    return res.status(200).json({success: 'Password changed successfully.'});
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'Error querying database.'});
+  }
+}
+
+exports.changeEmail = async (req, res) => {
+  const {email} = req.body;
+  const {user_id} = req.session;
+
+  if (email == null) {
+    return res.status(400).json({error: 'Email cannot be empty.'});
+  }
+
+  try {
+    await db.send_sql(
+      `UPDATE users SET email = '${email}' WHERE user_id = ${user_id}`
+    );
+    return res.status(200).json({success: 'Email changed successfully.'});
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'Error querying database.'});
+  }
+}
+
+exports.changeActor = async (req, res) => {
+  const { user_id } = req.session;
+  const { img_id } = req.body;
+
+  // Update the img_id field in the users table
+  try {
+    // Check if the user exists
+    const user = await db.send_sql(
+      `SELECT * FROM users WHERE user_id = ${user_id}`
+    );
+    if (user.length === 0) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    await db.send_sql(
+      `UPDATE users SET img_id = ${img_id} WHERE user_id = ${user_id}`
+    );
+    return res.status(200).json({ success: 'Actor changed successfully.' });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Error querying database.' });
+  }
+}
+
+exports.getFiveClosestActors = async (req, res) => {
+  const { user_id } = req.session;
+
+  // Find the actor_id associated with the current user
+  try {
+    const actor_id = await db.send_sql(
+      `SELECT actor_id FROM users WHERE user_id = ${user_id}`
+    );
+    if (actor.length === 0) {
+      return res.status(404).json({error: 'Actor not found.'});
+    }
+
+    //// TODO: Find the 5 closest embeddings
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'Error querying database.'});
+  }
+}

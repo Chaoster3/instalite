78bd39a0c7e223cdf35aac61452773d009f415e6
Ethan Yu <ethayu1@gmail.com>
2024-04-23 21:26:15 -0400

    add profile photo + jank hashtag for signup

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Signup({ onSignIn }) {
+  const [isFirstPage, setIsFirstPage] = useState(true);
+
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -11,6 +13,8 @@ function Signup({ onSignIn }) {
     email: "",
     affiliation: "",
     birthday: "",
+    profilePicture: null,
+    hashtags: "",
   });
 
   const handleChange = (e) => {
@@ -21,9 +25,8 @@ function Signup({ onSignIn }) {
     }));
   };
 
-  const handleSignup = (e) => {
+  const handleSignup1 = (e) => {
     e.preventDefault();
-    //TODO: make POST request to /register to sign up
     if (!formData.email.includes("@") && !formData.email.includes(".")) {
       alert("Invalid email");
       return;
@@ -49,6 +52,12 @@ function Signup({ onSignIn }) {
       return;
     }
     console.log("Sign Up clicked");
+    setIsFirstPage(false);
+  };
+
+  const handleSignup2 = (e) => {
+    //TODO: make POST request to /register to sign up
+    e.preventDefault();
     onSignIn();
   };
 
@@ -60,38 +69,87 @@ function Signup({ onSignIn }) {
     { name: "birthday", placeholder: "Birthday", type: "date" },
     { name: "username", placeholder: "Username", type: "text" },
     { name: "password", placeholder: "Password", type: "password" },
-    { name: "passwordConfirm", placeholder: "Confirm Password", type: "password" },
+    {
+      name: "passwordConfirm",
+      placeholder: "Confirm Password",
+      type: "password",
+    },
   ];
 
   return (
     <div className="h-screen w-screen flex justify-center flex-col text-gray-700 w-96 rounded-xl bg-clip-border mx-auto">
       <div className="text-center text-2xl font-bold mb-4">
-        Welcome to Myelin Oligodendrocyte Glycoprotein
+        {isFirstPage
+          ? "Welcome to Myelin Oligodendrocyte Glycoprotein"
+          : "Personalize Your Profile"}
       </div>
       <div className="flex flex-col gap-4 p-6">
-        {inputFields.map((field, index) => (
-          <div key={index} className="relative h-11 w-full min-w-[200px]">
-            <input
-              type={field.type}
-              name={field.name}
-              value={formData[field.name]}
-              onChange={handleChange}
-              placeholder=" "
-              className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-            />
-            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-              {field.placeholder}
-            </label>
-          </div>
-        ))}
+        {isFirstPage &&
+          inputFields.map((field, index) => (
+            <div key={index} className="relative h-11 w-full min-w-[200px]">
+              <input
+                type={field.type}
+                name={field.name}
+                value={formData[field.name]}
+                onChange={handleChange}
+                placeholder=" "
+                className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                {field.placeholder}
+              </label>
+            </div>
+          ))}
+        {!isFirstPage && (
+          <>
+            <div key={8} className="relative h-11 w-full min-w-[200px]">
+              <input
+                type="file"
+                accept="image/*"
+                capture="user"
+                name="Profile Picture"
+                onChange={(e) => {
+                  console.log(e.target.files);
+                  setFormData((prevFormData) => ({
+                    ...prevFormData,
+                    profilePicture: e.target.files[0],
+                  }));
+                }}
+                placeholder=" "
+                className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                {"Profile Photo"}
+              </label>
+            </div>
+            <div key={9} className="relative h-11 w-full min-w-[200px]">
+              <input
+                type="text"
+                name="Hashtags"
+                value={formData["hashtags"]}
+                onChange={(e) => {
+                  setFormData((prevFormData) => ({
+                    ...prevFormData,
+                    hashtags: e.target.value,
+                  }));
+                }}
+                placeholder=" "
+                className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                {"Hashtags"}
+              </label>
+            </div>
+          </>
+        )}
       </div>
       <div className="p-6 pt-0">
         <button
-          onClick={handleSignup}
+          onClick={isFirstPage ? handleSignup1 : handleSignup2}
           className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
         >
-          Sign Up
+          {isFirstPage ? "Next" : "Sign Up"}
         </button>
         <p className="flex justify-center mt-6 font-sans text-sm antialiased font-light leading-normal text-inherit">
           Already have an account?{" "}

68fe918489c53aea68605fd0e036e9f6463e3a8a
Chaoster3 <samuelngiam@yahoo.com>
2025-08-23 17:03:20 -0400

    small styling

--- frontend/src/components/auth/ForgotPassword.jsx
+++ frontend/src/components/auth/ForgotPassword.jsx
@@ -53,13 +53,15 @@ function ForgotPassword() {
                     </label>
                 </div>
             </div>
-            <button
-                onClick={handleSubmit}
-                className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                type="button"
-            >
-                Send Email
-            </button>
+            <div className="px-6">
+                <button
+                    onClick={handleSubmit}
+                    className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    type="button"
+                >
+                    Send Email
+                </button>
+            </div>
             <div className="p-6 pt-0 mt-4">
                 <Link
                     to="/login"
